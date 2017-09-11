3d3fffd03b9edd34e451a16e5da1fdfa89a16e6b
Tung Vu <tungvu3196@gmail.com>
2017-09-11 13:25:31 -0700

    Update files.

---
+++ PythonTutorial/Exercises/Exercises/exercise-1/ex1.py
@@ -0,0 +1,3 @@
+print("Hello world!")
+print("My name is Tung")
+print(1+1)
---
+++ PythonTutorial/Exercises/Exercises/exercise-2/ex2.py
@@ -0,0 +1,6 @@
+# A comment, this is so you can read your program later.
+
+print("I could have code like this.")
+print("Hi # there.")
+# You can also use a comment to "disable" or comment out code
+print("This will run.")
