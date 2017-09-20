2c2e5388a4dff53dc7610e1c895c34cedb624363
tungvu3196 <tungvu3196@gmail.com>
2017-09-20 14:20:39 +0700

    add new exercises

---
+++ PythonTutorial/Exercises/Exercises/exercise-34/ex34.py
@@ -0,0 +1,6 @@
+# Exercise 34: Accessing Elements of Lists
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("The first animal is at %d is %s" % (0, animals.pop(0)))
+print("The third animal is at %d is %s" % (2, animals.pop(2)))
