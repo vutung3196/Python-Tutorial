f7e2ae54176ebba45f0389c0a4de897603de7682
tungvu3196 <tungvu3196@gmail.com>
2017-09-20 17:11:00 +0700

    Add new exercies.

--- PythonTutorial/Exercises/Exercises/exercise-34/ex34.py
+++ PythonTutorial/Exercises/Exercises/exercise-34/ex34.py
@@ -2,5 +2,6 @@
 
 animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 
+# Here is my examples
 print("The first animal is at %d is %s" % (0, animals.pop(0)))
 print("The third animal is at %d is %s" % (2, animals.pop(2)))
