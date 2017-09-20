5dc5907b32c637c697a46db875502edad69999b4
tungvu3196 <tungvu3196@gmail.com>
2017-09-20 09:42:37 +0700

    Update exercise 25 and 26.

--- PythonTutorial/Exercises/Exercises/exercise-25/ex25.py
+++ PythonTutorial/Exercises/Exercises/exercise-25/ex25.py
@@ -35,28 +35,3 @@ def print_first_and_last_sorted(sentence):
     words = sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)
-
-# Use break_words method
-a = break_words("Hello, I'm Tung")
-print(a)
-
-# Use sort_words method
-b = sort_words("abd, dsd")
-print(b)
-
-# Use another sort_words method example
-c = sort_words("dcba, tung vu123")
-print(c)
-
-# Use two print-word methods
-print_first_word(a)
-print_last_word(c)
-
-# Use sort_sentence method
-d = sort_sentence("Hello world!, I'm Vu Tung")
-print(d)
-
-# Use two print-word methods
-print_first_and_last("Hello world!, I'm Vu Tung")
-
-print_first_and_last_sorted("Hello world!, I'm Vu Tung")
