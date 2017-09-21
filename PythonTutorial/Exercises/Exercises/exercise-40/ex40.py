b7450ba5ae294d329c16fcbef8092f10ced2e734
tungvu3196 <tungvu3196@gmail.com>
2017-09-21 09:57:14 +0700

    Add new exercises.

--- PythonTutorial/Exercises/Exercises/exercise-40/ex40.py
+++ PythonTutorial/Exercises/Exercises/exercise-40/ex40.py
@@ -4,7 +4,7 @@ class Song(object):
 
     def __init__(self, lyrics):
         self.lyrics = lyrics
-  
+        
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
